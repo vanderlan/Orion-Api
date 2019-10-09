aac016a7fce53d5f48ef22b2900ab8beb0e96985
Vanderlan Gomes <vanderlan.gs@outlook.com>
2019-10-09 15:20:30 -0300

    .

--- VBaseProject.Api/Startup.cs
+++ VBaseProject.Api/Startup.cs
@@ -3,7 +3,6 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Localization;
