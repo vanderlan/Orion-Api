a78302230355578582c9949127863c3c514afdfd
Vanderlan Gomes <vanderlan.gs@outlook.com>
2020-04-23 12:07:54 -0300

    Add Swagger Authentication Option

--- VBaseProject.Api/Startup.cs
+++ VBaseProject.Api/Startup.cs
@@ -75,6 +75,36 @@ namespace VBaseProject
                     Title = "VBaseProject API",
                     Description = "VBase Project - API Description Here!"
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
             });
             #endregion
 
