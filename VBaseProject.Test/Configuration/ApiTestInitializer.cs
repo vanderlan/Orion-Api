4354fb723194098bfe24de46c36b34c003a93f28
Vanderlan Gomes <vanderlan.gs@gmail.com>
2021-02-13 21:36:15 -0300

    Initial API Test Setup

---
+++ VBaseProject.Test/Configuration/ApiTestInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+
+namespace VBaseProject.Test.Configuration
+{
+    public abstract class ApiTestInitializer : WebApplicationFactory<Startup>
+    {
+        protected HttpClient _client;
+
+        public ApiTestInitializer()
+        {
+            Setup();
+        }
+
+        public void Setup()
+        {
+            var builder = new WebHostBuilder();
+
+            base.ConfigureWebHost(builder);
+
+            _client = Server.CreateClient();
+        }
+       
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Development")
+                .UseStartup<Startup>();
+
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
