74bcfd3a6c1082861c6c86fb6c2786bb7b314fd6
Vanderlan Gomes da Silva <vanderlan.gs@gmail.com>
2022-12-27 21:24:18 -0300

    Fix security issues related to JWT Auth

---
+++ VBaseProject.Api/Jwt/JwtConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace VBaseProject.Api.Jwt
+{
+    public class JwtConfiguration
+    {
+        public string SymmetricSecurityKey { get; set; } 
+        public string Issuer { get; set; }
+        public string Audience { get; set; } 
+        public int TokenExpirationMinutes { get; set; }
+    }
+}
--- VBaseProject.Domain/Authentication/AuthenticationConfiguration.cs
+++ VBaseProject.Domain/Authentication/AuthenticationConfiguration.cs
@@ -1,16 +1,7 @@
 namespace VBaseProject.Domain.Authentication
 {
-    public static class AuthenticationConfiguration
+    public static class AuthorizationConfiguration
     {
-        public static class Jwt
-        {
-            public const string SymmetricSecurityKey = "5cCI6IkpXVCJ9.eyJlbWFpbCI6InZhbmRlcmxhbi5nc0BnbWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJhZG1p";
-            public const string Issuer = "http://www.myapplication.com";
-            public const string Audience = "http://www.myapplication.com";
-
-            public const int TokenExpirationMinutes = 10;
-        }
-
         public static class Roles
         {
             public const string Admin = "admin";
