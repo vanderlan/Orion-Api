259c9c146e3ccb3b00488ec72c4dff51cc4b6751
83585-Vanderlan Silva <vanderlan.gs@gmail.com>
2023-10-10 16:32:49 -0300

    Add Elastic Search and SQL Server Health checks

---
+++ Orion.Api/Configuration/HealthCheckConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Orion.Api.Configuration
+{
+    public static class HealthCheckConfiguration
+    {
+        public static void ConfigureHealthCheck(this IApplicationBuilder app) 
+        {
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        }
+
+        public static void AddApplicationHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks().AddSqlServer(configuration["DatabaseOptions:ConnectionString"],
+                tags: new[] 
+                { 
+                    "database"
+                });
+
+            services.AddHealthChecks().AddElasticsearch(configuration["ElasticConfiguration:Uri"],
+                tags: new[] 
+                { 
+                    "elasticsearch", 
+                    "kibana" 
+                });
+        }
+    }
+}
