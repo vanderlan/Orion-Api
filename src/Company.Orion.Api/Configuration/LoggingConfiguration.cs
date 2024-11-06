96de823c15ee9af9654a6dacde9631974d4e3af2
Vanderlan Gomes da Silva <vanderlan.gs@gmail.com>
2024-11-06 13:38:41 -0300

    Update Packages

--- src/Company.Orion.Api/Configuration/LoggingConfiguration.cs
+++ src/Company.Orion.Api/Configuration/LoggingConfiguration.cs
@@ -1,5 +1,9 @@
-﻿using Serilog;
-using Serilog.Sinks.Elasticsearch;
+﻿using Elastic.Channels;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
+using Serilog;
 
 namespace Company.Orion.Api.Configuration;
 
@@ -14,18 +18,20 @@ public static class LoggingConfiguration
             .Enrich.WithCorrelationIdHeader()
             .WriteTo.Console(outputTemplate: "[{CorrelationId} - {Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
             .WriteTo.Debug()
-            .WriteTo.Elasticsearch(ConfigureElasticSink(configuration))
+            .WriteTo.Elasticsearch([new Uri(configuration["ElasticConfiguration:Uri"])], opts =>
+            {
+                opts.BootstrapMethod = BootstrapMethod.Failure;
+                opts.DataStream = new DataStreamName("logs", configuration["Serilog:IndexFormat"]);
+                opts.ConfigureChannel = channelOpts => {
+                    channelOpts.BufferOptions = new BufferOptions { ExportMaxConcurrency = 10 };
+                };
+            }, transport =>
+            {
+                transport.Authentication(new BasicAuthentication("elastic", "change"));
+            })
             .Enrich.WithProperty("AppName", configuration["Serilog:AppName"])
             .ReadFrom.Configuration(configuration)
             .CreateLogger();
     }
 
-    private static ElasticsearchSinkOptions ConfigureElasticSink(IConfiguration configuration)
-    {
-        return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
-        {
-            AutoRegisterTemplate = true,
-            IndexFormat = configuration["Serilog:IndexFormat"]
-        };
-    }
 }
