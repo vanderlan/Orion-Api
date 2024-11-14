0977a2118e7885fa57a14fc180a3a5d56f47a3cd
Vanderlan Gomes <vanderlan.gs@gmail.com>
2024-11-14 12:00:42 -0300

    Fix pipeline starting ElasticSerach Service (#157)

--- src/Company.Orion.Api/Program.cs
+++ src/Company.Orion.Api/Program.cs
@@ -5,15 +5,11 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.ConfigureLogging();
-
 builder.Host.UseSerilog();
 
 builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
-
-app.UseSerilogRequestLogging();
 
 app.UseMiddleware<OrionMiddleware>();
 
